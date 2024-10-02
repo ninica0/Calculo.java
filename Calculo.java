75272d17980b03986e49738fd795b73a9a136023
ninica0 <santosnico048@gmail.com>
2024-10-02 14:21:16 -0300

    feat: Sistema de cálculo com if e else

---
+++ Calculo.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class Calculo {
+	public static void main(String[] args) {
+
+int a, b, c, d, x;
+		
+		a= 2;
+				b = 3;
+				c=5;
+				d=9;
+				
+				//Processamento
+				if (d > 5) {
+					x = (a + b) * d;
+				}else {
+					x = (a-b)/c;
+				}
+				
+				System.out.println("O resultado do cálculo é: " + x);
+				}
+	}
